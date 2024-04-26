42349fca1d8b944107c9dfdb17287dfdbbaaafad
Joshua Eze <jezeadiebuo5@gmail.com>
2024-04-26 15:32:14 +0100

    final commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,6 @@
 import CollectionComponent from "@/components/collections/Collection";
 import Hero from "@/components/hero/Hero";
-import Navbar from "@/components/navbar/Navbar";
 import ShortcutComponent from "@/components/shortcut/Shortcut";
-import dynamic from "next/dynamic";
-
 
 export default function Home() {
   return (
