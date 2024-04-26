0ec64979619c1e8c6300b8561bc7678b717d4a7a
Joshua Eze <jezeadiebuo5@gmail.com>
2024-04-26 15:44:08 +0100

    made new changes to Hero component

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import CollectionComponent from "@/components/collections/Collection";
 import Hero from "@/components/hero/Hero";
 import ShortcutComponent from "@/components/shortcut/Shortcut";
+import dynamic from "next/dynamic";
+
 
 export default function Home() {
   return (
--- components/hero/Hero.tsx
+++ components/hero/Hero.tsx
@@ -6,7 +6,15 @@ import { Banner } from "@/data/response/banner";
 import BannerSlider from "./BannerSlider";
 import dynamic from "next/dynamic";
 
-const DelayedBannerSlider = dynamic(() => import("./BannerSlider"));
+
+const DelayedBannerSlider = dynamic(() => import("./BannerSlider"), {
+  loading: () => (
+    <div className="flex flex-row justify-center h-12 bg-white items-center w-full">
+      <p className="text-2xl text-black">Loading...</p>
+    </div>
+  ),
+  
+});
 
 export default function Hero() {
   const [bannerData, setBannerData] = useState<Banner[]>([]);
