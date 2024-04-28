5615b9518217069ea4fb411e6a53288832b247fc
Joshua Eze <jezeadiebuo5@gmail.com>
2024-04-28 08:54:49 +0100

    updated BanneSlider component

--- components/hero/Hero.tsx
+++ components/hero/Hero.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 
 import bannerRepo from "@/data/repository/banner/banner-repo";
 import { useEffect, useState } from "react";
@@ -6,20 +6,11 @@ import { Banner } from "@/data/response/banner";
 import BannerSlider from "./BannerSlider";
 import dynamic from "next/dynamic";
 
-
-const DelayedBannerSlider = dynamic(() => import("./BannerSlider"), {
-  loading: () => (
-    <div className="flex flex-row justify-center h-12 bg-white items-center w-full">
-      <p className="text-2xl text-black">Loading...</p>
-    </div>
-  ),
-  
-});
+const DelayedBannerSlider = dynamic(() => import("./BannerSlider"));
 
 export default function Hero() {
   const [bannerData, setBannerData] = useState<Banner[]>([]);
   useEffect(() => {
-    
     async function getBannerData() {
       const data = await bannerRepo.getBanners();
       setBannerData(data);
@@ -28,10 +19,5 @@ export default function Hero() {
     getBannerData();
   }, []);
 
-
-  return (
-    <DelayedBannerSlider
-      data={bannerData}
-    />
-  );
+  return <DelayedBannerSlider data={bannerData} />;
 }
