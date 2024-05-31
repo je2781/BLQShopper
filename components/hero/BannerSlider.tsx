e383a89c2a738d09fdbfe7fee67d14f6a52dc119
Joshua Eze <jezeadiebuo5@gmail.com>
2024-05-31 21:01:33 +0100

    another commit

--- components/hero/BannerSlider.tsx
+++ components/hero/BannerSlider.tsx
@@ -84,7 +84,7 @@ export default function BannerSliderComponent({ data,  }: sliderProps) {
                 <img
                   src={datum.pcImageUrl}
                   alt="banner image"
-                  className="object-cover "
+                  className="object-cover lg:h-fit w-full"
                 />
               </Link>
             </div>
